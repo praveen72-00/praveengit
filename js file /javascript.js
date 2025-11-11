43e8e32009373ab5f13add4d9371eb3bb35d3f9c
praveen <praveenthiru00@gmail.com>
2025-11-11 13:26:27 +0530

    all the staged code

---
+++ js file /javascript.js
@@ -0,0 +1,86 @@
+/*console.log("welcome to the js file of javascript praveen");-----multi line */
+//console .log ("hola praveen "); ---- single line 
+
+//variable
+// let bookname ;
+//  bookname="praveen";
+// console.log (bookname);
+
+// let inifi= Infinity;
+// console.log (inifi);
+//  let infi = 23;
+// console.log(infi);---------------------------------------
+
+// variable constant
+// const sum= 556+23;
+// console.log (typeof sum );--------------------------------
+
+// type converstion
+
+// let age =23;
+// console .log (typeof age);
+
+// age =String(age);
+// console.log (age );
+
+// let vote=String (true) ;
+// console.log(vote);
+
+// condition -------------------------
+
+// if condition 
+
+// let age ;
+// let eligbleToVote;
+
+// age =15 ;
+// if (age >20){
+//     eligbleToVote =true;
+// }
+// else {
+//     eligbleToVote= false;
+// }
+// console .log (eligbleToVote)
+
+// else if condition ---------
+
+// let age ;
+// let eligbleToVote;
+
+// age =0;
+// if (age >=18){
+//     eligbleToVote ="major";
+// }
+// else if (age>1){
+//     eligbleToVote= "Minor";
+// }
+// else{
+//     eligbleToVote ="child";
+// }
+// console .log (eligbleToVote)
+
+// let year =2016;--------------------- nested if 
+// if (year % 4 ==0 ){
+//     if (year % 100 ==0){
+//         if (year % 400 ==0){
+//             console.log ('it is a leap year');
+
+//         }
+//         else {
+//             console.log('not an leap year');
+//         }
+
+//     }
+//     else {
+//         console .log ("it is an leap year");
+//     }
+    
+// }
+// else{
+//     console.log('not a leap year');
+// }
+
+
+
+
+
---
+++ loops.js
@@ -0,0 +1,28 @@
+// let  num ;---- while loop 
+// num =1;
+// while (num<=10){
+//     console .log (num);
+//     num++
+// }.   < less than // greater than > /// 5 is then 10
+ 
+// for (let i=10;i>2;i--){//-----for loop
+//     console.log (i);
+// }
+
+// let num=1;
+// let fac=5;
+// for (let i=1;i<=fac;i++){
+//     num=num*i;
+// }
+// console.log(num);
+
+let num =1;
+let fac=5;
+let i=1;
+
+while (i<=fac){
+   
+    num=num*i;
+     i++;
+}
+console.log(num );
